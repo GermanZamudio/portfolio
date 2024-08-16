861cc9f9d8c15f071384b8c55041ee7ed8507cc9
German Zamudio <germanbenjamin@hotmail.com>
2024-08-16 00:59:23 -0300

    modificacion staticfiels_dirs

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -129,7 +129,7 @@ USE_TZ = True
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATICFILES_DIRS = [os.path.join(BASE_DIR,'Home/static')]
 
 if not DEBUG:
     # Tell Django to copy static assets into a path called `staticfiles` (this is specific to Render)
