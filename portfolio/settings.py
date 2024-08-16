c25142c8bace9a5c7d8539f162261a8f599c42cc
German Zamudio <germanbenjamin@hotmail.com>
2024-08-16 01:58:11 -0300

    prueba

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -84,16 +84,16 @@ WSGI_APPLICATION = 'portfolio.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 
-#DATABASES = {
-#    'default': {
-#        'ENGINE': 'django.db.backends.sqlite3',
-#        'NAME': BASE_DIR / 'db.sqlite3',
-#    }
-#}
-
 DATABASES = {
-    'default':dj_database_url.parse(os.environ.get('DATABASE_URL'), conn_max_age=600),
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
+
+#DATABASES = {
+#    'default':dj_database_url.parse(os.environ.get('DATABASE_URL'), conn_max_age=600),
+#}
 
 #DATABASES = {
 #    'default': dj_database_url.config(
